c8bdb4d4a303ab2d015c171f822f7e004cdc3efe
tylerwarre <tyler@warrens.one>
2026-07-02 13:14:15 -0400

    Added vim pentesting modules

--- lua/modules/init.lua
+++ lua/modules/init.lua
@@ -1,3 +1,4 @@
 require("modules.lsp")
 require("modules.options")
 require("modules.mappings")
+require("modules.pentesting")
--- lua/modules/mappings.lua
+++ lua/modules/mappings.lua
@@ -4,3 +4,7 @@ vim.g.mapleader = " "
 local telescope = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', telescope.find_files, { desc = 'Telescope: find files' })
 vim.keymap.set('n', '<leader>fs', telescope.live_grep, { desc = 'Telescope: search files' })
+
+-- Checkbox
+vim.keymap.set('n', '<leader>cc', "<cmd>normal! 0t]rx<CR>")
+vim.keymap.set('n', '<leader>cu', "<cmd>normal! 0t]r<Space><CR>")
