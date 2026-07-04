55fa194df9e0211953644bb62278d2c835326a93
tylerwarre <tyler@warrens.one>
2026-07-03 23:06:16 -0600

    v0.12.3: Added 'hl' to lua_ls LSP globals

--- lsp/lua_ls.lua
+++ lsp/lua_ls.lua
@@ -65,31 +65,31 @@
 ---
 
 local root_markers1 = {
-    '.emmyrc.json',
-    '.luarc.json',
-    '.luarc.jsonc',
+	'.emmyrc.json',
+	'.luarc.json',
+	'.luarc.jsonc',
 }
 local root_markers2 = {
-    '.luacheckrc',
-    '.stylua.toml',
-    'stylua.toml',
-    'selene.toml',
-    'selene.yml',
+	'.luacheckrc',
+	'.stylua.toml',
+	'stylua.toml',
+	'selene.toml',
+	'selene.yml',
 }
 
 ---@type vim.lsp.Config
 return {
-    cmd = { 'lua-language-server' },
-    filetypes = { 'lua' },
-    root_markers = vim.fn.has('nvim-0.11.3') == 1 and { root_markers1, root_markers2, { '.git' } }
-    or vim.list_extend(vim.list_extend(root_markers1, root_markers2), { '.git' }),
-    ---@type lspconfig.settings.lua_ls
-    settings = {
-        Lua = {
-            codeLens = { enable = true },
-            hint = { enable = true, semicolon = 'Disable' },
+	cmd = { 'lua-language-server' },
+	filetypes = { 'lua' },
+	root_markers = vim.fn.has('nvim-0.11.3') == 1 and { root_markers1, root_markers2, { '.git' } }
+		or vim.list_extend(vim.list_extend(root_markers1, root_markers2), { '.git' }),
+	---@type lspconfig.settings.lua_ls
+	settings = {
+		Lua = {
+			codeLens = { enable = true },
+			hint = { enable = true, semicolon = 'Disable' },
 			-- required for lua_ls to recognize 'vim' in config
-			diagnostics = { globals = { 'vim' } },
-        },
-    },
+			diagnostics = { globals = { 'vim', 'hl' } },
+		},
+	},
 }
