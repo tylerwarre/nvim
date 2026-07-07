7def5f27afe26c237249123f28c4069830fabab0
tylerwarre <tyler@warrens.one>
2026-07-06 22:03:19 -0600

    v0.12.3: Chagned LSP error symbol to ascii square

--- lua/modules/options.lua
+++ lua/modules/options.lua
@@ -10,10 +10,10 @@ vim.opt.completeopt = { "menu", "menuone", "noselect" }
 vim.diagnostic.config({
 	signs = {
 		text = {
-			[vim.diagnostic.severity.ERROR] = '',
-			[vim.diagnostic.severity.WARN]  = '',
-			[vim.diagnostic.severity.INFO]  = '',
-			[vim.diagnostic.severity.HINT]  = ''
+			[vim.diagnostic.severity.ERROR] = '■',
+			[vim.diagnostic.severity.WARN]  = '■',
+			[vim.diagnostic.severity.INFO]  = '■',
+			[vim.diagnostic.severity.HINT]  = '■'
 		}
 	}
 })
