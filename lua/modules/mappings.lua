0a69b28658d7babc580faf4bebc816f6196f1f93
tylerwarre <tyler@warrens.one>
2026-07-24 00:41:22 -0700

    v0.12.4: Added telescope mapping error handling

--- lua/modules/mappings.lua
+++ lua/modules/mappings.lua
@@ -1,10 +1,15 @@
 vim.g.mapleader = " "
 
 -- Telescope
-local telescope = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', telescope.find_files, { desc = 'Telescope: find files' })
-vim.keymap.set('n', '<leader>fg', telescope.git_files, { desc = 'Telescope: find files' })
-vim.keymap.set('n', '<leader>fs', telescope.live_grep, { desc = 'Telescope: search files' })
+local status, telescope pcall(function()
+	return require('telescope.builtin')
+end)
+
+if status then
+	vim.keymap.set('n', '<leader>ff', telescope.find_files, { desc = 'Telescope: find files' })
+	vim.keymap.set('n', '<leader>fg', telescope.git_files, { desc = 'Telescope: find files' })
+	vim.keymap.set('n', '<leader>fs', telescope.live_grep, { desc = 'Telescope: search files' })
+end
 
 -- Debug
 vim.keymap.set('n', '<leader>db', function() vim.cmd("Break") end, { desc = 'Debug: toggle breakpoint on current line' })
